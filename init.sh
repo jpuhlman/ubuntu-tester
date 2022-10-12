d9a87c59658c2a0364f6321fdeed45cb77ce0521
Jeremy A. Puhlman <jpuhlman@mvista.com>
2022-10-12 13:32:33 -0700

    Update init.sh for repoes

--- init.sh
+++ init.sh
@@ -1,41 +1,19 @@
 #!/bin/bash
 
-if [ -e /etc/yum.repos.d-overlay ] ; then
-   if [ ! -L /etc/yum.repos.d ] ; then
-        mv /etc/yum.repos.d /etc/yum.repos.d-save
-        mkdir -p /etc/yum.repos.d
-        cp /etc/yum.repos.d-overlay/* /etc/yum.repos.d
-   fi
-fi
 if [ -d local-repos ] ; then
-    for repo in /local-repos/*; do
-        REPONAME=$(basename $repo)
-        cat > /etc/yum.repos.d/local-$REPONAME.repo << EOF
-[localrepo-$REPONAME]
-name=Local repo $REPONAME
-failovermethod=priority
-enabled=1
-gpgcheck=0
-baseurl=file://$repo
-EOF
+    for repo in local-repos/*; do 
+        for release in $repo/dists/*; do
+            RELEASE=$(dirname $release)
+            for section in $release/*; do
+                if [ -d $section ] ; then
+                    SECTION=$(dirname $section)
+                    echo "deb file://$repo $release $SECTION" >> /etc/apt/sources.list.d/montavista.list
+                fi
+            done
+        done
     done
 fi
 
-if [ -n "$EXTRA_REPOS" ] ; then
-    for repo in $EXTRA_REPOS; do 
-        REPONAME=$(echo $repo | sed s,\",,g | cut -d "/" -f 4- | sed s,/,_,g | sed s,_$,,)
-
-cat > /etc/yum.repos.d/$REPONAME.repo << EOF
-[$REPONAME]
-name=$REPONAME
-failovermethod=priority
-enabled=1
-gpgcheck=0
-baseurl=$repo
-EOF
-
-    done
-fi
 if [ -n "$SYSTEMD" ] ; then
     exec /sbin/init
 else
